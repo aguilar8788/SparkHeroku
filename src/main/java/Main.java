a6eff4606044b7fdfb12412de462c391fc108dc5
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-08-03 15:47:33 -0600

    spark hello world example

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by peteraguilar on 8/3/16.
+ */
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+}
+
+
